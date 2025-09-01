de8da894100a7ecf86654bfc3c5ea35ece21b554
omarseddati16 <omarseddati16@gmail.com>
2025-09-01 14:26:51 +0200

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import films from "../data/films";
+import { useState } from "react";
+
+
+
+
+const Main = () => {
+
+  const [search, setSearch] = useState("");
+  const [searchFilm, setSearchFilm] = useState(films)
+
+  useEffect(() => {
+
+    const filteredFilms = films.filter((film) =>
+      film.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSearchFilm(filteredFilms);
+  }, [search]);
+
+
+  const handleForm = (event) => {
+    event.preventDefault();
+
+  }
+
+  return (
+    <>
+      <form onSubmit={handleForm}>
+        <input
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button className='btn  btn-primary'>Cerca</button>
+      </form>
+
+      <ul className="list-unstyled">
+        {searchFilm.map((filmsMapped, index) => (
+          <li className="m-5" key={index}>
+            <h5>{filmsMapped.title}</h5>
+            <p>{filmsMapped.genre}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Main;
---
+++ data/films.js
@@ -0,0 +1,11 @@
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+
+export default films
